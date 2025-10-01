313b63ee3ee899576a33ec0b612f463e9875cfdf
Cauã <cauafreirw@gmail.com>
2025-10-01 16:43:20 -0300

    1- Criando o projeto loja

---
+++ ProjetoLoja2dsa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoLoja2dsa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
